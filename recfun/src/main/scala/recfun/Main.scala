d10fff3871eea323ebeea220aa9ef9e2a864688e
ronalddehaan <rodhaan@wehkamp.nl>
2018-01-09 19:42:23 +0100

    Implement tests week 1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,37 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+//    def pascal(c: Int, r: Int): Int = (c,r) match {
+//      case (0,_) => 1
+//      case (_,0) => 1
+//      case (c,r) => pascal(c-1,r-1) + pascal(c, r-1)
+//    }
+  def pascal(c: Int, r: Int): Int = {
+    if (c == r || c == 0 || r == 0) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def inner(count: Int, chars: List[Char]): Boolean = {
+        if (count < 0) false
+        else if (count == 0 && chars.isEmpty) true
+        else if (count != 0 && chars.isEmpty) false
+        else if (chars.head.equals(')')) inner(count - 1, chars.tail)
+        else if (chars.head.equals('(')) inner(count + 1, chars.tail)
+        else inner(count, chars.tail)
+      }
+
+      inner(0, chars)
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int =
+      if (money == 0) 1
+      else if (coins.isEmpty || money < 0) 0
+      else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
